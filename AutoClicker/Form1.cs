e66d1d59f27b18268f22ee9bfbc42ceb85d05435
ArthurPRR <arthur.pierre86@gmail.com>
2026-08-25 22:13:14 +0200

    add icon

--- AutoClicker/Form1.cs
+++ AutoClicker/Form1.cs
@@ -99,7 +99,9 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
-        
+        this.Icon = new Icon(
+            Path.Combine(AppContext.BaseDirectory, "resources", "icon.ico")
+        );
         AcceptButton = null;
         
         InitializeDefaults();
